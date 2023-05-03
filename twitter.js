a3eb375e0135a814c9fcded737c60296977f2507
ummagohil <umgo0510@gmail.com>
2023-05-03 10:06:58 +0100

    twitter followers removed (public and private)

--- twitter.js
+++ twitter.js
@@ -1,13 +1,19 @@
 function hideTwitterNumbers() {
   const followers = document.querySelectorAll("div.css-1dbjc4n.r-1mf7evn");
-  const followings = document.querySelectorAll(
+  const followingsPublic = document.querySelectorAll(
     "a.css-4rbku5.css-18t94o4.css-901oao.r-18jsvk2.r-1loqt21.r-37j5jr.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-qvutc0"
+  );
+  const followingsPrivate = document.querySelectorAll(
+    "div.css-1dbjc4n.r-13awgt0.r-18u37iz.r-1w6e6rj"
   );
   followers.forEach((follower) => {
     follower.style.display = "none";
   });
-  followings.forEach((following) => {
-    following.style.display = "none";
+  followingsPublic.forEach((followingsPublic) => {
+    followingsPublic.style.display = "none";
+  });
+  followingsPrivate.forEach((followingsPrivate) => {
+    followingsPrivate.style.display = "none";
   });
 }
 
