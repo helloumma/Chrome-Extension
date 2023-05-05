cf3ca26b8faaccefecb9fd768f2234690cbc0123
ummagohil <umgo0510@gmail.com>
2023-05-05 09:11:31 +0100

    fix

--- twitter.js
+++ twitter.js
@@ -3,7 +3,7 @@ function hideTwitterNumbers() {
     "span.css-901oao.css-16my406.r-18jsvk2.r-poiln3.r-1b43r93.r-b88u0q.r-1cwl3u0.r-bcqeeo.r-qvutc0"
   );
   const likes = document.querySelectorAll(
-    "div.css-901oao.css-1hf3ou5.r-1bwzh9t.r-37j5jr.r-n6v787.r-16dba41.r-1cwl3u0.r-bcqeeo.r-qvutc0"
+    "div.css-901oao.css-1hf3ou5.r-14j79pv.r-37j5jr.r-n6v787.r-16dba41.r-1cwl3u0.r-bcqeeo.r-qvutc0"
   );
   followersFollowing.forEach((followersFollowing) => {
     followersFollowing.style.display = "none";
